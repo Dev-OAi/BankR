14931b0c7b04299b1cb806bbec1dd39d9512cb16
GameDev <54653160+OlegMenyuk@users.noreply.github.com>
2025-08-02 22:30:25 -0400

    feat: Add Sallie Mae Bank to scraper

--- fetch-data.js
+++ fetch-data.js
@@ -10,7 +10,8 @@ const TARGET_BANKS = [
   { name: 'Chase', url: 'https://www.depositaccounts.com/banks/chase-manhattan-bank.html' },
   { name: 'Valley Bank', url: 'https://www.depositaccounts.com/banks/valley.html' },
   { name: 'Flagstar Bank', url: 'https://www.depositaccounts.com/banks/flagstar-bank.html' },
-  { name: 'TD Bank', url: 'https://www.depositaccounts.com/banks/td-bank-national-association.html' }
+  { name: 'TD Bank', url: 'https://www.depositaccounts.com/banks/td-bank-national-association.html' },
+  { name: 'Sallie Mae Bank', url: 'https://www.depositaccounts.com/banks/sallie-mae-bank.html' }
 ];
 const HISTORY_DIR = './public/history';
 const LATEST_RATES_FILE = './public/latest_rates.json';
